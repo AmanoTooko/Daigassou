ed25e8fa23dfcc2c4572e141a908ad48ed11244c
Yuu Morino <YMorino@fairylab.org>
2018-03-11 22:13:57 +0800

    Add keyboard simulation

--- Daigassou/Form1.cs
+++ Daigassou/Form1.cs
@@ -12,9 +12,30 @@ namespace Daigassou
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            
+            HotKeys.Regist(new Window().Handle,(int)HotKeys.HotkeyModifiers.None,Keys.F4, testFunction);
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void testFunction()
+        {
+            Queue<KeyPlayList> a=new Queue<KeyPlayList>();
+            a.Enqueue(new KeyPlayList(Keys.W,1));
+            a.Enqueue(new KeyPlayList(Keys.S, 1));
+            a.Enqueue(new KeyPlayList(Keys.Space, 2));
+
+            Task t=new  Task(() => { KeyController.KeyPlayBack(a, 75); });
+            t.Start();
+            
+        }
+
     }
 }
---
+++ Daigassou/KeyController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Daigassou
+{
+    public class KeyController
+    {
+
+        [DllImport("User32.dll")]
+        public static extern void keybd_event(Keys bVk, Byte bScan, Int32 dwFlags, Int32 dwExtraInfo);
+        [DllImport("user32.dll")]
+        static extern uint MapVirtualKey(uint uCode, uint uMapType);
+
+        Stopwatch sw = new Stopwatch();
+        
+        public KeyController()
+        {
+            
+        }
+
+        public static void KeyboardPress(Keys viKeys)
+        {
+                Console.WriteLine($"{viKeys.ToString()} has been pressed at {Environment.TickCount}");
+                keybd_event(viKeys, (byte)MapVirtualKey((uint)viKeys, 0), 0, 0);
+                Thread.Sleep(3);
+                keybd_event(viKeys, (byte)MapVirtualKey((uint)viKeys, 0), 2, 0);
+        }
+
+        public static void KeyPlayBack(Queue<KeyPlayList> keyQueue,int tick)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            int startTime = Environment.TickCount;
+            long targetTime = startTime;
+            int duration = 0;
+                while (keyQueue.Any() == true && !cts.Token.IsCancellationRequested)
+                {
+                    KeyPlayList nextKey = keyQueue.Dequeue();
+                    duration=(int)(tick * nextKey.Tick);
+                    targetTime = startTime + duration;
+                    while (true)
+                    {
+                        if (targetTime<=Environment.TickCount)
+                        {
+                            break;
+                        }
+                    }
+
+                    startTime = Environment.TickCount;
+                    Console.WriteLine($" i called function at {startTime} with target time is {targetTime}");
+                    KeyboardPress(nextKey.Key);
+                }
+  
+            
+        }
+
+
+    }
+    public class KeyPlayList
+    {
+        public KeyPlayList(Keys key,uint tick)
+        {
+            Tick = tick;
+            Key =key;
+        }
+
+        public uint Tick;
+        public Keys Key;
+    }
+}
