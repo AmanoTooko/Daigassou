dd5e8119baccc51f968903772a00a8fb3c3b94d1
Shiro <shiro@ffxiv.cat>
2022-11-19 22:50:07 +0800

    Merge from DX

---
+++ Daigassou/Input_Midi/MidiFileParser.cs
@@ -0,0 +1,428 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Interaction;
+using Melanchall.DryWetMidi.Multimedia;
+using Melanchall.DryWetMidi.Tools;
+
+namespace Daigassou.Controller
+{
+    public class MidiFileParser
+    {
+        private readonly bool autoChord;
+        private readonly uint MIN_DELAY_TIME_MS_CHORD;
+        private readonly uint MIN_DELAY_TIME_MS_EVENT;
+        private readonly List<TimedObjectsManager<Note>> tracks;
+        public int Index;
+        private MidiFile midi;
+
+        private OutputDevice outputDevice;
+        private Playback playback;
+        private TempoMap Tmap;
+        private List<TrackChunk> trunks;
+
+
+        public MidiFileParser()
+        {
+            tracks = new List<TimedObjectsManager<Note>>();
+            Bpm = 80;
+            MIN_DELAY_TIME_MS_EVENT = Properties.Settings.Default.MinChordMs;
+            MIN_DELAY_TIME_MS_CHORD = Properties.Settings.Default.MinChordMs;
+            autoChord = true;//.Settings.Default.isAutoChord;
+        }
+
+        public int Bpm { get; set; }
+
+        public void OpenFile(byte[] s)
+        {
+            midi = MidiFile.Read(new MemoryStream(s), new ReadingSettings
+            {
+                NoHeaderChunkPolicy = NoHeaderChunkPolicy.Ignore,
+                NotEnoughBytesPolicy = NotEnoughBytesPolicy.Ignore,
+                InvalidChannelEventParameterValuePolicy = InvalidChannelEventParameterValuePolicy.ReadValid,
+                InvalidChunkSizePolicy = InvalidChunkSizePolicy.Ignore,
+                InvalidMetaEventParameterValuePolicy = InvalidMetaEventParameterValuePolicy.SnapToLimits,
+                MissedEndOfTrackPolicy = MissedEndOfTrackPolicy.Ignore,
+                UnexpectedTrackChunksCountPolicy = UnexpectedTrackChunksCountPolicy.Ignore,
+                ExtraTrackChunkPolicy = ExtraTrackChunkPolicy.Read,
+                UnknownChunkIdPolicy = UnknownChunkIdPolicy.ReadAsUnknownChunk,
+                SilentNoteOnPolicy = SilentNoteOnPolicy.NoteOff,
+                TextEncoding = Encoding.Default,
+                InvalidSystemCommonEventParameterValuePolicy = InvalidSystemCommonEventParameterValuePolicy.SnapToLimits
+            });
+            Tmap = midi.GetTempoMap();
+        }
+
+        public void OpenFile(string path)
+        {
+            midi = MidiFile.Read(path, new ReadingSettings
+            {
+                NoHeaderChunkPolicy = NoHeaderChunkPolicy.Ignore,
+                NotEnoughBytesPolicy = NotEnoughBytesPolicy.Ignore,
+                InvalidChannelEventParameterValuePolicy = InvalidChannelEventParameterValuePolicy.ReadValid,
+                InvalidChunkSizePolicy = InvalidChunkSizePolicy.Ignore,
+                InvalidMetaEventParameterValuePolicy = InvalidMetaEventParameterValuePolicy.SnapToLimits,
+                MissedEndOfTrackPolicy = MissedEndOfTrackPolicy.Ignore,
+                UnexpectedTrackChunksCountPolicy = UnexpectedTrackChunksCountPolicy.Ignore,
+                ExtraTrackChunkPolicy = ExtraTrackChunkPolicy.Read,
+                UnknownChunkIdPolicy = UnknownChunkIdPolicy.ReadAsUnknownChunk,
+                SilentNoteOnPolicy = SilentNoteOnPolicy.NoteOff,
+                TextEncoding = Encoding.Default
+            });
+            Tmap = midi.GetTempoMap();
+        }
+
+        /// <summary>
+        ///     Get a String list of track name from trackmanager
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetTrackNames()
+        {
+            try
+            {
+                tracks.Clear();
+                var score = new List<string>();
+                trunks = new List<TrackChunk>();
+                foreach (var track in midi.GetTrackChunks())
+                    if (track.ManageNotes().Objects.Count() != 0)
+                    {
+                        tracks.Add(track.ManageNotes());
+                        trunks.Add(track);
+                    }
+
+                  
+
+                for (var i = 0; i < trunks.Count; i++)
+                {
+                    var name = "Untitled";
+                    foreach (var trunkEvent in trunks[i].Events)
+                        if (trunkEvent is SequenceTrackNameEvent)
+                        {
+                            var e = trunkEvent as SequenceTrackNameEvent;
+                            name = e.Text;
+                            break;
+                        }
+
+                    score.Add(name);
+                }
+
+
+                return score;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"这个Midi文件读取出错！请使用其他软件重新保存。\r\n异常信息：{e.Message}\r\n 异常类型{e.GetType()}",
+                    "读取错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+
+        /// <summary>
+        ///     和弦处理，主要逻辑：
+        ///     | ===
+        ///     | ===
+        ///     | ===
+        ///     1. 将N个Note组成的chord拆成 从Chord.time起，每隔LENGTH-N时间演奏一个note
+        ///     |     =
+        ///     |   ==
+        ///     | ===
+        ///     2. 在chord.time前后按照最小时间均匀分布N个note
+        ///     | =
+        ///     中
+        ///     =|
+        /// </summary>
+        public void PreProcessChord()
+        {
+            using (var chordManager = new ChordsManager(trunks.ElementAt(Index).Events))
+            {
+                foreach (var chord in chordManager.Chords)
+                    if (chord.Notes.Count() > 1)
+                    {
+                        var count = 0;
+                        if (autoChord)
+                        {
+                            var autoTick = chord.Length / (chord.Notes.Count() + 1);
+                            foreach (var note in chord.Notes.OrderBy(x => x.NoteNumber))
+                            {
+                                note.Time += count * autoTick;
+                                note.Length = autoTick;
+                                count++;
+                            }
+                        }
+                        else
+                        {
+                            var lenoffset =
+                                LengthConverter.ConvertFrom(new MetricTimeSpan(MIN_DELAY_TIME_MS_CHORD * 1000),
+                                    chord.Time, Tmap);
+                            var startTime = chord.Time - lenoffset * (chord.Notes.Count() - 1) / 2 < 0
+                                ? 0
+                                : chord.Time - lenoffset * (chord.Notes.Count() - 1) / 2;
+                            var len = chord.Length / chord.Notes.Count();
+                            foreach (var note in chord.Notes.OrderBy(x => x.NoteNumber))
+                            {
+                                note.Length = len;
+                                note.Time = startTime + count * lenoffset;
+                                count++;
+                            }
+                        }
+                    }
+            }
+        }
+
+        /// <summary>
+        ///     Event处理，主要逻辑：遇到同音符连续按下时，提前offevent
+        /// </summary>
+        public void PreProcessEvents()
+        {
+            using (var notesManager = trunks.ElementAt(Index).ManageNotes())
+            {
+                Note lastNote = null;
+                foreach (var note in notesManager.Objects)
+                {
+                    var lastNoteStartTime = lastNote?.TimeAs(TimeSpanType.Metric, Tmap) as MetricTimeSpan;
+                    var lastNoteLength = lastNote?.LengthAs(TimeSpanType.Metric, Tmap) as MetricTimeSpan;
+                    var currentNoteLength = note.TimeAs(TimeSpanType.Metric, Tmap) as MetricTimeSpan;
+
+                    if (lastNote != null &&
+                        lastNoteStartTime.TotalMicroseconds + lastNoteLength.TotalMicroseconds +
+                        MIN_DELAY_TIME_MS_EVENT * 1000 > currentNoteLength.TotalMicroseconds)
+                    {
+                        var minTick = LengthConverter.ConvertFrom(new MetricTimeSpan(MIN_DELAY_TIME_MS_EVENT * 1000),
+                            lastNote.Time, Tmap);
+                        if (lastNoteLength.TotalMicroseconds < MIN_DELAY_TIME_MS_EVENT * 1000 ||
+                            note.Time - lastNote.Time <= minTick)
+                        {
+                            ///  ===       =>     ===
+                            ///======   =>   ={}
+                            lastNote.Length =
+                                LengthConverter.ConvertFrom(new MetricTimeSpan(MIN_DELAY_TIME_MS_EVENT * 1000),
+                                    lastNote.Time, Tmap);
+                            
+                        }
+                        else
+                        {
+                            ///
+                            ///            ===    =>               ===
+                            ///=======        =>   ======
+                            ///
+                            var noteSpan = note.TimeAs(TimeSpanType.Metric, Tmap)
+                                .Subtract(lastNoteStartTime, TimeSpanMode.TimeTime);
+                            lastNote.Length = LengthConverter.ConvertFrom(
+                                noteSpan.Subtract(new MetricTimeSpan(MIN_DELAY_TIME_MS_EVENT * 1000),
+                                    TimeSpanMode.LengthLength), lastNote.Time, Tmap);
+                        }
+                    }
+
+                    lastNote = note;
+                }
+            }
+        }
+
+        /// <summary>
+        ///     清除水果或其他软件产生的duration小于10ms的音
+        /// </summary>
+        public void PreProcessNoise()
+        {
+            using (var eventsManager = trunks.ElementAt(Index).Events.ManageNotes())
+            {
+                foreach (var @event in eventsManager.Objects)
+                    if ((@event.LengthAs(TimeSpanType.Metric, Tmap) as MetricTimeSpan).TotalMicroseconds < 10000.0)
+                        eventsManager.Objects.Remove(@event);
+            }
+        }
+
+        public void SaveToFile()
+        {
+            // PreProcessTempoMap();
+            for (var i = 0; i < trunks.Count; i++)
+            {
+                Index = i;
+                PreProcessNoise();
+                PreProcessChord();
+                PreProcessEvents();
+            }
+
+            var stfd = new SaveFileDialog();
+            stfd.Filter = "Midi文件|*.mid";
+            if (stfd.ShowDialog() == DialogResult.OK)
+                midi.Write(stfd.FileName, false, MidiFileFormat.MultiTrack,
+                    new WritingSettings {TextEncoding = Encoding.Default});
+        }
+
+        public Queue<KeyPlayList> ArrangeKeyPlays(double speed)
+        {
+            var trunkEvents = trunks.ElementAt(Index).Events;
+
+            var ticksPerQuarterNote = Convert.ToInt64(midi.TimeDivision.ToString()
+                .TrimEnd(" ticks/qnote".ToCharArray()));
+
+            var nowTimeMs = 0.0;
+            var retKeyPlayLists = new Queue<KeyPlayList>();
+            PreProcessNoise();
+            PreProcessSpeed(speed);
+            PreProcessChord();
+            PreProcessEvents();
+            using (var timedEvent = trunkEvents.ManageTimedEvents())
+            {
+                foreach (var ev in timedEvent.Objects)
+                    switch (ev.Event)
+                    {
+                        case NoteOnEvent @event:
+                        {
+                            var noteNumber = (int) @event.NoteNumber;
+                            nowTimeMs = (ev.TimeAs(TimeSpanType.Metric, Tmap) as MetricTimeSpan).TotalMicroseconds /
+                                        1000.0;
+                            retKeyPlayLists.Enqueue(new KeyPlayList(KeyPlayList.NoteEvent.NoteOn,
+                                noteNumber, nowTimeMs));
+                        }
+                            break;
+                        case NoteOffEvent @event:
+                        {
+                            var noteNumber = (int) @event.NoteNumber;
+                            nowTimeMs = (ev.TimeAs(TimeSpanType.Metric, Tmap) as MetricTimeSpan).TotalMicroseconds /
+                                        1000.0;
+                            retKeyPlayLists.Enqueue(new KeyPlayList(KeyPlayList.NoteEvent.NoteOff,
+                                noteNumber, nowTimeMs));
+                        }
+                            break;
+                    }
+            }
+
+            return retKeyPlayLists;
+        }
+
+        public Playback GetPlayback()
+        {
+            PreProcessNoise();
+
+            PreProcessChord();
+            PreProcessEvents();
+            var playback = new Playback(trunks.ElementAt(Index).GetNotes(), Tmap);
+            return playback;
+        }
+
+        public Playback GetPlaybackForAll()
+        {
+            for (int i = 0; i < trunks.Count; i++)
+            {
+                Index = i;
+                PreProcessNoise();
+                PreProcessChord();
+                PreProcessEvents();
+            }
+
+            var playback = midi.GetPlayback();
+            return playback;
+        }
+
+        public void PreProcessSpeed(double speed)
+        {
+            using (var eventsManager = trunks.ElementAt(Index).Events.ManageTimedEvents())
+            {
+                foreach (var @event in eventsManager.Objects) @event.Time = (long) (@event.Time * speed);
+            }
+        }
+
+        //public void PreProcessTempoMap()
+        //{
+        //    foreach (var trackChunk in trunks)
+        //        using (var eventsManager = trackChunk.Events.ManageTimedEvents())
+        //        {
+        //            foreach (var tmpChange in Tmap.GetTempoChanges())
+        //            {
+        //                var ev = new SetTempoEvent(tmpChange.Value.MicrosecondsPerQuarterNote);
+        //                eventsManager.Events.AddEvent(ev, tmpChange.Time);
+        //            }
+        //        }
+        //}
+
+        //public int GetBpm()
+        //{
+        //    var bpm = (int)Tmap.Tempo.AtTime(0).BeatsPerMinute;
+        //    return bpm;
+        //}
+
+        #region MidiFilePlayBack
+
+        public int PlaybackPause()
+        {
+            if (playback == null) return -1;
+
+            if (!playback.IsRunning) return -2;
+            playback.Stop();
+            return 0;
+        }
+
+        //public int PlaybackStart(int BPM, EventHandler playbackFinishHandler)
+        //{
+        //    if (midi == null) return -1;
+
+        //    if (OutputDevice.GetDevicesCount() == 0) return -2;
+        //    if (outputDevice == null && (outputDevice = OutputDevice.GetByName("Microsoft GS Wavetable Synth")) == null)
+        //        outputDevice = OutputDevice.GetAll().ElementAt(0);
+        //    if (playback == null)
+        //        playback = new Playback(trunks.ElementAt(Index).Events, midi.GetTempoMap(), outputDevice);
+        //    playback.Speed = (double)BPM / GetBpm();
+        //    playback.InterruptNotesOnStop = true;
+        //    playback.Start();
+        //    playback.Finished += playbackFinishHandler;
+
+        //    return 0;
+        //}
+
+
+        //public int PlaybackPercentGet()
+        //{
+        //    if (playback.IsRunning)
+        //    {
+        //        var cur = (MidiTimeSpan)playback.GetCurrentTime(TimeSpanType.Midi);
+        //        var dur = (MidiTimeSpan)playback.GetDuration(TimeSpanType.Midi);
+
+        //        return (int)(cur.TimeSpan * 100 / dur.TimeSpan);
+        //    }
+        //    return 0;
+        //}
+        //public void PlaybackPercentSet(int process)
+        //{
+        //    if (playback.IsRunning)
+        //    {
+        //        MidiTimeSpan dur = (MidiTimeSpan)playback.GetDuration(TimeSpanType.Midi);
+
+        //        var tar = new MidiTimeSpan(dur.TimeSpan * process / 100);
+        //        playback.MoveToTime(tar);
+
+        //    }
+        //}
+        //public string PlaybackInfo()
+        //{
+        //    var ret = "";
+        //    var expression = @"\d*:(?<time>.+):\d+";
+        //    if (playback.IsRunning)
+        //    {
+        //        var cur = playback.GetCurrentTime(TimeSpanType.Metric);
+        //        var dur = playback.GetDuration(TimeSpanType.Metric);
+
+        //        ret = Regex.Match(cur.ToString(), expression).Groups["time"].Value + "/" +
+        //              Regex.Match(dur.ToString(), expression).Groups["time"].Value;
+        //    }
+        //    return ret;
+        //}
+
+        //public int PlaybackRestart()
+        //{
+        //    if (midi == null) return -1;
+        //    if (playback == null) return -2;
+
+        //    playback.Stop();
+        //    playback.Dispose();
+        //    playback = null;
+        //    return 0;
+        //}
+
+        #endregion
+    }
+}
---
+++ Daigassou/Utils/Instrument.cs
@@ -0,0 +1,259 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Daigassou.Utils
+{
+    public class Instrument
+    {
+        public static readonly Dictionary<int, string> InstrumentName = new Dictionary<
+            int,
+            string
+        >
+        {
+            {0, "妹有乐器"},
+            {1, "竖琴"},
+            {2, "钢琴"},
+            {3, "鲁特琴"},
+            {4, "提琴拨弦"},
+            {5, "长笛"},
+            {6, "双簧管"},
+            {7, "单簧管"},
+            {8, "横笛"},
+            {9, "排箫"},
+            {10, "定音鼓"},
+            {11, "邦戈鼓"},
+            {12, "低音鼓"},
+            {13, "小军鼓"},
+            {14, "镲"},
+            {15, "小号"},
+            {16, "长号"},
+            {17, "大号"},
+            {18, "圆号"},
+            {19, "萨克斯管"},
+            {20, "小提琴"},
+            {21, "中提琴"},
+            {22, "大提琴"},
+            {23, "重音提琴"},
+            {24, "过载吉他"},
+            {25, "清音吉他"},
+            {26, "闷音吉他"},
+            {27, "强力和弦"},
+            {28, "装逼吉他"}
+        };
+
+        public static readonly Dictionary<string, int> InstrumentAlias = new Dictionary<
+            string,
+            int
+        >
+        {
+            {"竖琴", 1},
+            {"shu qin", 1},
+            {"shuqin", 1},
+            {"harp", 1},
+            {"hrp", 1},
+            {"钢琴", 2},
+            {"gang qin", 2},
+            {"gangqin", 2},
+            {"piano", 2},
+            {"grand piano", 2},
+            {"grand_piano", 2},
+            {"grandpiano", 2},
+            {"pno", 2},
+            {"鲁特琴", 3},
+            {"lu te qin", 3},
+            {"luteqin", 3},
+            {"steelguitar", 3},
+            {"steel guitar", 3},
+            {"steel_guitar", 3},
+            {"guitar", 3},
+            {"lute", 3},
+            {"lt", 3},
+            {"鲁特", 3},
+            {"fiddle", 3},
+            {"提琴拨弦", 4},
+            {"ti qin bo xian", 4},
+            {"tiqinboxian", 4},
+            {"pizzicato", 4},
+            {"拨弦", 4},
+            {"pizz", 4},
+            {"长笛", 5},
+            {"chang di", 5},
+            {"changdi", 5},
+            {"flute", 5},
+            {"fl", 5},
+            {"双簧管", 6},
+            {"shuang huang guan", 6},
+            {"shuanghuangguan", 6},
+            {"oboe", 6},
+            {"ob", 6},
+            {"单簧管", 7},
+            {"dan huang guan", 7},
+            {"danhuangguan", 7},
+            {"clarinet", 7},
+            {"cl", 7},
+            {"横笛", 8},
+            {"heng di", 8},
+            {"hengdi", 8},
+            {"piccolo", 8},
+            {"fife", 8},
+            {"picc", 8},
+            {"排箫", 9},
+            {"pai xiao", 9},
+            {"paixiao", 9},
+            {"panflute", 9},
+            {"panpipe", 9},
+            {"箫", 9},
+            {"定音鼓", 10},
+            {"ding yin gu", 10},
+            {"dingyingu", 10},
+            {"timpani", 10},
+            {"timp", 10},
+            {"邦戈鼓", 11},
+            {"bang ge gu", 11},
+            {"banggegu", 11},
+            {"bongo", 11},
+            {"低音鼓", 12},
+            {"di yin gu", 12},
+            {"diyingu", 12},
+            {"bassdrum", 12},
+            {"bd", 12},
+            {"底鼓", 12},
+            {"小军鼓", 13},
+            {"xiao jun gu", 13},
+            {"xiaojungu", 13},
+            {"snare", 13},
+            {"jungu", 13},
+            {"军鼓", 13},
+            {"sd", 13},
+            {"镲", 14},
+            {"cha", 14},
+            {"cymbal", 14},
+            {"擦", 14},
+            {"cym", 14},
+            {"小号", 15},
+            {"xiao hao", 15},
+            {"xiaohao", 15},
+            {"trumpet", 15},
+            {"tpt", 15},
+            {"长号", 16},
+            {"chang hao", 16},
+            {"changhao", 16},
+            {"trombone", 16},
+            {"tbn", 16},
+            {"大号", 17},
+            {"da hao", 17},
+            {"dahao", 17},
+            {"tuba", 17},
+            {"tba", 17},
+            {"圆号", 18},
+            {"yuan hao", 18},
+            {"yuanhao", 18},
+            {"horn", 18},
+            {"frenchhorn", 18},
+            {"hn", 18},
+            {"萨克斯管", 19},
+            {"sa ke si guan", 19},
+            {"sakesiguan", 19},
+            {"altosax", 19},
+            {"saxophone", 19},
+            {"saxo", 19},
+            {"萨克斯", 19},
+            {"sax", 19},
+            {"小提琴", 20},
+            {"xiao ti qin", 20},
+            {"xiaotiqin", 20},
+            {"violin", 20},
+            {"vln", 20},
+            {"中提琴", 21},
+            {"zhong ti qin", 21},
+            {"zhongtiqin", 21},
+            {"viola", 21},
+            {"vla", 21},
+            {"大提琴", 22},
+            {"da ti qin", 22},
+            {"datiqin", 22},
+            {"cello", 22},
+            {"低音", 22},
+            {"vc", 22},
+            {"低音提琴", 23},
+            {"di yin ti qin", 23},
+            {"diyintiqin", 23},
+            {"contrabass", 23},
+            {"double bass", 23},
+            {"bass", 23},
+            {"doublebass", 23},
+            {"cb", 23},
+            {"电吉他,过载", 24},
+            {"guo zai", 24},
+            {"guozai", 24},
+            {"overdriven", 24},
+            {"过载", 24},
+            {"OdGuitar", 24},
+            {"egod", 24},
+            {"ele_overdriven", 24},
+            {"ele_over", 24},
+            {"电吉他,清音", 25},
+            {"qing yin", 25},
+            {"qingyin", 25},
+            {"clean", 25},
+            {"清音", 25},
+            {"cleanguitar", 25},
+            {"guitarclean", 25},
+            {"egcl", 25},
+            {"ele_clean", 25},
+            {"电吉他,闷音", 26},
+            {"men yin", 26},
+            {"menyin", 26},
+            {"mute", 26},
+            {"闷音", 26},
+            {"muteguitar", 26},
+            {"egm", 26},
+            {"ele_mute", 26},
+            {"电吉他,重力和弦", 27},
+            {"zhong li he xian", 27},
+            {"zhonglihexian", 27},
+            {"powerchord", 27},
+            {"重力", 27},
+            {"GGuitar", 27},
+            {"egpc", 27},
+            {"ele_powerchord", 27},
+            {"电吉他,特殊奏法", 28},
+            {"te shu zou fa", 28},
+            {"teshuzoufa", 28},
+            {"special", 28},
+            {"ele_special", 28},
+            {"特殊", 28},
+            {"egfx", 28}
+        };
+
+        public int IntruID { get; set; }
+
+        public override string ToString()
+        {
+            if (InstrumentName.Keys.Contains(IntruID))
+                return InstrumentName[IntruID];
+            return InstrumentName[0];
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="name">alias to instrucode </param>
+        /// <returns>convert success</returns>
+        public bool AliasToCode(string name)
+        {
+            var clearedName = name.ToLower();
+            clearedName = Regex.Replace(clearedName, @"\[.*\]|\{.*\}|\(.*\)|【.*】|（.*）", ""); //FIlter []【】（）(){}
+
+
+            if (InstrumentAlias.ContainsKey(clearedName))
+            {
+                IntruID = InstrumentAlias[clearedName];
+                return true;
+            }
+
+            IntruID = 0;
+            return false;
+        }
+    }
+}
